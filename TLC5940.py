16cea886668f36c618bb4cacbb82f9247ef8c460
Tyler Ward <Tyler@scorpia.co.uk>
2013-05-10 21:13:43 +0100

    changed the tests.
    Code is now tested and working on a pi

--- TLC5940.py
+++ TLC5940.py
@@ -1,27 +1,50 @@
 #!/usr/bin/python
 
 import RPi.GPIO as GPIO
-import spidev
+#import spidev
+import spi
 import time
+from threading import Thread
 
 XLAT=4
 VPRG=17
+
+BLANK=23
+GSCLK=18
 
 # setup rpi.gpio pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(XLAT, GPIO.OUT)
 GPIO.setup(VPRG, GPIO.OUT)
 
+GPIO.setup(BLANK, GPIO.OUT)
+GPIO.setup(GSCLK, GPIO.OUT)
+
 # setup serial
 
-spi = spidev.SpiDev()
-spi.open(0,0)
+#spi = spidev.SpiDev()
+#spi.open(0,0)
+
+spi.openSPI(speed=500000)
 
 #constants
 
 # VPRG settings for registers
 regDC=1
 regPWM=0
+
+def runGSCLK():
+	i=0
+	while(1):
+		i+=1
+		if i>=1096:
+			print "reset"
+			GPIO.output(BLANK,1)
+			GPIO.output(BLANK,0)
+			i=0
+		else:
+			GPIO.output(GSCLK,1)
+			GPIO.output(GSCLK,0)
 
 def buildvalue(values, register):
 
@@ -41,24 +64,25 @@ def buildvalue(values, register):
 
 	# for every element in array 
 	for output in range(len(values)):
-		print str(output) + " = " + str(values[output])
-		print bin(values[output])
+		#print str(output) + " = " + str(values[output])
+		#print bin(values[output])[2:].zfill(reglength)
+		#print reglength
 		for char in bin(values[output])[2:].zfill(reglength)[::-1]:
 			if char=="1":
 				bitstream.append(1)
 			elif char =="0":
 				bitstream.append(0)
 	#print bitstream
-	print bitstream[::-1]
+	#print bitstream[::-1]
 	for bitoffset in range(0,len(bitstream),8):
 		outbyte=0
 		for bit in range(8):
 			if bitstream[::-1][bitoffset+bit]==1:
 				outbyte+=2**(7-bit)
-		print outbyte
+		#print outbyte
 		out.append(outbyte)
 
-	print out
+	#print out
 	return out			
 				
 def setTLCvalue(data,DCMode):
@@ -66,11 +90,11 @@ def setTLCvalue(data,DCMode):
 	# put the chip into DC mode	
 	GPIO.output(VPRG, DCMode)
 	
-	print spi.xfer2(data)
+	print spi.transfer(tuple(data))
 
 	# latch data
 	GPIO.output(XLAT, GPIO.HIGH)
-	time.sleep(0.025)
+	#time.sleep(0.025)
 	GPIO.output(XLAT, GPIO.LOW)
 
 	GPIO.output(VPRG, GPIO.LOW)
@@ -78,7 +102,7 @@ def setTLCvalue(data,DCMode):
 
 def resetTLC():
 	# resets the chip to its default state
-	setTLCvalue([255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255],regPWM)
+	setTLCvalue([255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255],regPWM)
 	setTLCvalue([255,255,255,255,255,255,255,255,255,255,255,255],regDC)	
 
 
@@ -86,13 +110,22 @@ if __name__ == "__main__":
 
 	resetTLC()
 	import math
-
 	i=0
+	for val in range(0,4096,100):
+		print val
+		setTLCvalue(buildvalue([val,val,val,val,val,val,val,val,val,val,val,val,val,val,val,val],regPWM),regPWM)
+		time.sleep(0.5)	
+
+	for val in range(4096):
+		print val
+		setTLCvalue(buildvalue([val,val,val,val,val,val,val,val],regPWM),regPWM)	
+		time.sleep(0.01)
+
 	# pulsate by changeing brightness
 	while(1):
-		val = int(63*(math.sin(float(i))+1)/2)
-		i+=0.1
+		val = int(4000*(math.sin(float(i))+1)/2)
+		i+=0.01
 		print val
-		setTLCvalue(buildvalue([val,val,val,val,val,val,val,val,val,val,val,val,val,val,val,val],regDC),regDC)	
-		time.sleep(0.1)
+		setTLCvalue(buildvalue([val,val,val,val,val,val,val,val,val,val,val,val,val,val,val,val],regPWM),regPWM)	
+		time.sleep(0.01)
 
